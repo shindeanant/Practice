3584dfb61a2bd1847262c0274c6ad64b317b6be8
shindeanant <shindeanant1997@gmail.com>
2021-09-17 00:09:41 +0530

    Sample

---
+++ Comparison.java
@@ -0,0 +1,21 @@
+package javafellowship.com;
+
+import java.util.Scanner;
+
+public class Comparison {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Enter value of x: ");
+		 int x = scan.nextInt();
+		 System.out.println("Enter value of y: ");
+		 
+		  int y = scan.nextInt();
+		 
+		  System.out.println(x!=y);
+
+	}
+
+}
